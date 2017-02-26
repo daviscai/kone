dc49d7bea802dfef110122d8f80844b097a93a1f
daviscai <caiwxiong@qq.com>
2017-02-26 22:34:35 +0800

    update testunit

--- src/config/router.js
+++ src/config/router.js
@@ -5,5 +5,6 @@ let router = new Router();
 
 router.get('/', Home.index);
 router.get('/home', Home.index);
+router.get('/home/testI18n', Home.testI18n);
 
 module.exports = router;
--- test/controllers/home-spec.js
+++ test/controllers/home-spec.js
@@ -4,7 +4,7 @@ import supertest from 'supertest';
 
 const appDir = path.resolve(__dirname, '../../');
 
-const app = require( appDir + '/app/index').default;
+const app = require( appDir + '/app/index');
 
 const req = supertest(app.listen());
 
@@ -19,9 +19,5 @@ test('home index', async () => {
 test.only('home i18n', async (t)=>{
     let res = await get('/home/testI18n');
     t.is(res.status, 200, 'testI18n is ok');
-    t.is(res.text, 'kwan 基于Koa2构建的Node.js开发框架', 'app.title=基于Koa2构建的Node.js开发框架');
-
-    let res2 = await get('/home/testI18n?locale=en');
-    t.is(res2.status, 200, 'testI18n is ok');
-    t.is(res2.text, 'kwan is a framework base on Koa2 for node.js', 'app.title=kwan is a framework base on Koa2 for node.js');
+    t.is(res.text, 'kone is a framework base on Koa2 for node.js', 'app.title=kone is a framework base on Koa2 for node.js');
 });
