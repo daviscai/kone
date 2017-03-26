a3c7200aca9314416f674031c20a954c07ae3d3e
daviscai <caiwxiong@qq.com>
2017-03-26 23:19:47 +0800

    add memeye monitor

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 // Overrides Node.js's require and compiles modules at runtime
 //require('babel-register');
 
+// cpu, memory monitor
+if( process.env.NODE_ENV !== 'production'){
+  const memeye = require('memeye');
+  memeye();
+}
 
 const port = 4000;
 const app = require('./app/index');
